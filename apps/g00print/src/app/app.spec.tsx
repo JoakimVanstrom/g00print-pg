068947eb1fd2852f823b80a7cca2208bdebca63d
Psykisk <joakim.vanstrom@gmail.com>
2023-05-17 23:48:25 +0200

    added testing

--- apps/g00print/src/app/app.spec.tsx
+++ apps/g00print/src/app/app.spec.tsx
@@ -1,15 +1,18 @@
 import { render } from '@testing-library/react';
-
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import store from './store/index';
 
 import App from './app';
 
 describe('App', () => {
   it('should render successfully', () => {
     const { baseElement } = render(
+      <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
+      </Provider>
     );
 
     expect(baseElement).toBeTruthy();
@@ -17,9 +20,11 @@ describe('App', () => {
 
   it('should have a greeting as the title', () => {
     const { getByText } = render(
+      <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
+      </Provider>
     );
 
     expect(getByText(/Welcome g00print/gi)).toBeTruthy();
