70cd79f8d0e2bdfb5510b0f4ca1fc1648eb6c291
Psykisk <joakim.vanstrom@gmail.com>
2023-05-18 10:15:53 +0200

    made a new base

---
+++ libs/g00print-lib/jest.config.ts
@@ -0,0 +1,10 @@
+/* eslint-disable */
+export default {
+  displayName: 'g00print-lib',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/libs/g00print-lib',
+};
